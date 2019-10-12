fd0fdc3e57157a9225fcbc641fa0883fdac5b31e
kanewood <13982177590@163.com>
2019-10-12 15:17:08 +0800

    update face idl-license.face-android

--- ih_patient/src/main/java/com/keydom/ih_patient/constant/Config.java
+++ ih_patient/src/main/java/com/keydom/ih_patient/constant/Config.java
@@ -9,12 +9,9 @@ public class Config {
     public static String apiKey = "tobubUAhwGvNFGhGRCXdszM6";
     public static String secretKey = "HXBxYnnsWHG8tRtiWVGPb0r9S4vNV867";
 
-
-    public static String licenseID = "tfzl-ih-patient-face-face-android";
+    //人脸识别的参数
+    public static String licenseID = "tfzl-ih-patient-face-android";
     public static String licenseFileName = "idl-license.face-android";
 
-    //人脸识别的参数
-    public static String testServiceLicenseID = "testservice-pat-face-face-android";
-    public static String testServiceLicenseFileName = "idl-license.face-android";
 
 }
