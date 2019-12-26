ef453d3fb4a500444e548dfa48d9b9b71ba6a03d
kanewood <13982177590@163.com>
2019-12-26 11:18:43 +0800

    feat(签约／解约):添加解约界面

--- ih_patient/src/main/java/com/keydom/ih_patient/activity/member/controller/ChargeMemberController.java
+++ ih_patient/src/main/java/com/keydom/ih_patient/activity/member/controller/ChargeMemberController.java
@@ -2,7 +2,6 @@ package com.keydom.ih_patient.activity.member.controller;
 
 import com.keydom.ih_common.base.ControllerImpl;
 import com.keydom.ih_patient.activity.member.view.ChargeMemberView;
-import com.keydom.ih_patient.activity.member.view.SignMemberView;
 
 public class ChargeMemberController extends ControllerImpl<ChargeMemberView> {
 }
