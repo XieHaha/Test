42089dec0f055bb045761f13631521cf428f0e4d
kanewood <13982177590@163.com>
2019-12-27 17:10:56 +0800

    feat(签约／续约):续约跳转逻辑

--- ih_patient/src/main/java/com/keydom/ih_patient/fragment/controller/TabMemberController.java
+++ ih_patient/src/main/java/com/keydom/ih_patient/fragment/controller/TabMemberController.java
@@ -5,6 +5,7 @@ import android.view.View;
 import com.keydom.ih_common.base.ControllerImpl;
 import com.keydom.ih_patient.R;
 import com.keydom.ih_patient.activity.member.ChargeMemberActivity;
+import com.keydom.ih_patient.activity.member.SignMemberActivity;
 import com.keydom.ih_patient.fragment.view.TabMemberView;
 
 public class TabMemberController extends ControllerImpl<TabMemberView> implements View.OnClickListener {
@@ -12,11 +13,12 @@ public class TabMemberController extends ControllerImpl<TabMemberView> implement
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.fragment_tab_member_open_now_tv:
-                ChargeMemberActivity.start(getContext());
+                SignMemberActivity.start(getContext());
                 break;
             case R.id.fragment_tab_member_func_charge_record_tv:
                 break;
             case R.id.fragment_tab_member_func_charge_tv:
+                ChargeMemberActivity.start(getContext());
                 break;
             default:
                 break;
