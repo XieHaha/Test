257ddc0b14bc0704fdf83c79b745e40796758fb4
kanewood <13982177590@163.com>
2020-01-09 14:38:59 +0800

    feat(会员):会员首页UI

--- ih_patient/src/main/java/com/keydom/ih_patient/fragment/TabMemberFragment.java
+++ ih_patient/src/main/java/com/keydom/ih_patient/fragment/TabMemberFragment.java
@@ -2,13 +2,11 @@ package com.keydom.ih_patient.fragment;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
 
 import com.keydom.ih_common.base.BaseControllerFragment;
 import com.keydom.ih_patient.R;
 import com.keydom.ih_patient.bean.Event;
 import com.keydom.ih_patient.constant.EventType;
-import com.keydom.ih_patient.constant.Global;
 import com.keydom.ih_patient.fragment.controller.TabMemberController;
 import com.keydom.ih_patient.fragment.view.TabMemberView;
 
@@ -20,9 +18,6 @@ import org.jetbrains.annotations.Nullable;
 
 public class TabMemberFragment extends BaseControllerFragment<TabMemberController> implements TabMemberView {
 
-    LinearLayout mDescRootLl;
-    LinearLayout mFuncRootLl;
-    LinearLayout mMemberInstRootLl;
 
     @Override
     public int getLayoutRes() {
@@ -33,17 +28,9 @@ public class TabMemberFragment extends BaseControllerFragment<TabMemberControlle
     public void onViewCreated(@NotNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        mMemberInstRootLl = view.findViewById(R.id.fragmnet_tab_member_member_inst_root_ll);
-        mFuncRootLl = view.findViewById(R.id.fragment_tab_member_func_root_ll);
-        mDescRootLl = view.findViewById(R.id.fragment_tab_member_desc_root_ll);
-
-        view.findViewById(R.id.fragment_tab_member_open_now_tv).setOnClickListener(getController());
-        view.findViewById(R.id.fragment_tab_member_func_charge_record_tv).setOnClickListener(getController());
-        view.findViewById(R.id.fragment_tab_member_func_charge_tv).setOnClickListener(getController());
-
-        memberLayoutShow();
-
         EventBus.getDefault().register(this);
+
+        view.findViewById(R.id.fragment_tab_member_get_vip_tv).setOnClickListener(getController());
     }
 
     @Override
@@ -67,19 +54,7 @@ public class TabMemberFragment extends BaseControllerFragment<TabMemberControlle
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onRecieve(Event event) {
         if (event.getType() == EventType.UPDATELOGINSTATE) {
-            memberLayoutShow();
         }
     }
 
-    private void memberLayoutShow() {
-        if(Global.isMember()){
-            mMemberInstRootLl.setVisibility(View.GONE);
-            mFuncRootLl.setVisibility(View.VISIBLE);
-            mDescRootLl.setVisibility(View.VISIBLE);
-        }else{
-            mMemberInstRootLl.setVisibility(View.VISIBLE);
-            mFuncRootLl.setVisibility(View.GONE);
-            mDescRootLl.setVisibility(View.GONE);
-        }
-    }
 }
--- ih_patient/src/main/java/com/keydom/ih_patient/fragment/controller/TabMemberController.java
+++ ih_patient/src/main/java/com/keydom/ih_patient/fragment/controller/TabMemberController.java
@@ -4,8 +4,6 @@ import android.view.View;
 
 import com.keydom.ih_common.base.ControllerImpl;
 import com.keydom.ih_patient.R;
-import com.keydom.ih_patient.activity.member.ChargeMemberActivity;
-import com.keydom.ih_patient.activity.member.ChargeMemberRecordActivity;
 import com.keydom.ih_patient.activity.member.SignMemberActivity;
 import com.keydom.ih_patient.fragment.view.TabMemberView;
 
@@ -13,14 +11,8 @@ public class TabMemberController extends ControllerImpl<TabMemberView> implement
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.fragment_tab_member_open_now_tv:
+            case R.id.fragment_tab_member_get_vip_tv:
                 SignMemberActivity.start(getContext());
-                break;
-            case R.id.fragment_tab_member_func_charge_record_tv:
-                ChargeMemberRecordActivity.start(getContext());
-                break;
-            case R.id.fragment_tab_member_func_charge_tv:
-                ChargeMemberActivity.start(getContext());
                 break;
             default:
                 break;
