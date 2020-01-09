5f1ad4570996e9073e1f7e54dd674bbde37a3740
kanewood <13982177590@163.com>
2020-01-09 17:17:07 +0800

    feat(会员):会员详情跳转逻辑

--- ih_patient/src/main/java/com/keydom/ih_patient/fragment/controller/VIPMemberDetailController.java
+++ ih_patient/src/main/java/com/keydom/ih_patient/fragment/controller/VIPMemberDetailController.java
@@ -4,8 +4,8 @@ import android.view.View;
 
 import com.keydom.ih_common.base.ControllerImpl;
 import com.keydom.ih_patient.R;
+import com.keydom.ih_patient.activity.member.ChargeMemberActivity;
 import com.keydom.ih_patient.activity.member.ChargeMemberRecordActivity;
-import com.keydom.ih_patient.activity.member.SignMemberActivity;
 import com.keydom.ih_patient.fragment.view.VIPMemberDetailView;
 
 public class VIPMemberDetailController extends ControllerImpl<VIPMemberDetailView> implements View.OnClickListener {
@@ -13,7 +13,7 @@ public class VIPMemberDetailController extends ControllerImpl<VIPMemberDetailVie
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.fragment_vip_member_detail_charge_ll:
-                SignMemberActivity.start(getContext());
+                ChargeMemberActivity.start(getContext());
                 break;
             case R.id.fragment_vip_member_detail_charge_record_ll:
                 ChargeMemberRecordActivity.start(getContext());
