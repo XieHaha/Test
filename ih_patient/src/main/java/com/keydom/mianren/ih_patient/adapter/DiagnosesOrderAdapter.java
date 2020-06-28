bf18905c6ef91ef5e50776cb056322c8ff6dba57
dundun <371523912@qq.com>
2020-06-28 17:20:25 +0800

    style[用户端]：1、问诊订单item布局、代码优化

--- ih_patient/src/main/java/com/keydom/mianren/ih_patient/adapter/DiagnosesOrderAdapter.java
+++ ih_patient/src/main/java/com/keydom/mianren/ih_patient/adapter/DiagnosesOrderAdapter.java
@@ -48,7 +48,7 @@ public class DiagnosesOrderAdapter extends BaseQuickAdapter<DiagnosesOrderBean, 
     public final int complete = 9;
     public final int triage = 11;
     public final int consultationWait = 13;
-    public final int consultationComplete= 14;
+    public final int consultationComplete = 14;
     public final int cancel = -1;
 
     /**
@@ -74,101 +74,113 @@ public class DiagnosesOrderAdapter extends BaseQuickAdapter<DiagnosesOrderBean, 
         CircleImageView headImg = helper.getView(R.id.doctor_head_img);
         GlideUtils.load(headImg, item.getAvatar() == null ? "" :
                 Const.IMAGE_HOST + item.getAvatar(), 0, 0, false, null);
-        TextView diagnoses_type_tv = helper.getView(R.id.diagnoses_type_tv);
-        LinearLayout order_complete_layout = helper.getView(R.id.order_complete_layout);
-        TextView back_diagnoses_tv = helper.getView(R.id.back_diagnoses_tv);
-        TextView change_diagnoses_tv = helper.getView(R.id.change_diagnoses_tv);
-        TextView diagnoses_doctor_change_tv = helper.getView(R.id.diagnoses_doctor_change_tv);
-        TextView wait_people_tv = helper.getView(R.id.wait_people_tv);
-        back_diagnoses_tv.setOnClickListener(new View.OnClickListener() {
+        TextView diagnosesTypeTv = helper.getView(R.id.diagnoses_type_tv);
+        LinearLayout orderCompleteLayout = helper.getView(R.id.order_complete_layout);
+        TextView backDiagnosesTv = helper.getView(R.id.back_diagnoses_tv);
+        TextView changeDiagnosesTv = helper.getView(R.id.change_diagnoses_tv);
+        TextView diagnosesDoctorChangeTv = helper.getView(R.id.diagnoses_doctor_change_tv);
+        TextView waitPeopleTv = helper.getView(R.id.wait_people_tv);
+        backDiagnosesTv.setOnClickListener(new View.OnClickListener() {
             @SingleClick(1000)
             @Override
             public void onClick(View view) {
-                if (iOnItemBtnClickListener != null)
+                if (iOnItemBtnClickListener != null) {
                     iOnItemBtnClickListener.onCancelDiagnosesClick(item);
+                }
             }
         });
         if (item.getState() == waiteRecieve && item.getWaitInquiryCount() > 0) {
-            wait_people_tv.setVisibility(View.VISIBLE);
-            wait_people_tv.setText("前面待诊人数:" + item.getWaitInquiryCount());
-        } else
-            wait_people_tv.setVisibility(View.GONE);
-        if (item.getState() == unPay || item.getState() == waiteRecieve || item.getState() == changDoctor)
-            back_diagnoses_tv.setVisibility(View.VISIBLE);
-        else
-            back_diagnoses_tv.setVisibility(View.GONE);
-        TextView pay_tv = helper.getView(R.id.pay_tv);
-        pay_tv.setOnClickListener(new View.OnClickListener() {
-            @SingleClick(1000)
-            @Override
-            public void onClick(View view) {
-                if (iOnItemBtnClickListener != null)
-                    iOnItemBtnClickListener.onPayClick(item);
-            }
-        });
-        if (item.getState() == unPay)
-            pay_tv.setVisibility(View.VISIBLE);
-        else if (item.getState() != unPay && item.getIsSubOrderUnPay() == 1)
-            pay_tv.setVisibility(View.VISIBLE);
-        else
-            pay_tv.setVisibility(View.GONE);
-
-       /* if(item.getState()==1)
-            wait_people_tv.setVisibility(View.VISIBLE);
-        else
-            wait_people_tv.setVisibility(View.GONE);*/
-        if (item.getState() == changDoctor)
-            change_diagnoses_tv.setVisibility(View.VISIBLE);
-        else
-            change_diagnoses_tv.setVisibility(View.GONE);
-
-        if (item.getState() == waiteChangeDiagnose)
-            diagnoses_doctor_change_tv.setVisibility(View.VISIBLE);
-        else
-            diagnoses_doctor_change_tv.setVisibility(View.GONE);
-        TextView comment_tv = helper.getView(R.id.comment_tv);
-        comment_tv.setOnClickListener(new View.OnClickListener() {
-            @SingleClick(1000)
-            @Override
-            public void onClick(View view) {
-                if (iOnItemBtnClickListener != null)
-                    iOnItemBtnClickListener.onCommentClick(item);
-            }
-        });
-
-        if (item.getState() == waiteEvaluate)
-            comment_tv.setVisibility(View.VISIBLE);
-        else
-            comment_tv.setVisibility(View.GONE);
-
-        LinearLayout state_layout = helper.getView(R.id.state_layout);
-        if (item.getState() == cancel) {
-            state_layout.setVisibility(View.VISIBLE);
-            TextView state_tv = helper.getView(R.id.state_tv);
-            if (item.getRefundState() == 0)
-                state_tv.setText("已取消");
-            else if (item.getRefundState() == 1)
-                state_tv.setText("退款中");
-            else
-                state_tv.setText("已退款");
-        } else
-            state_layout.setVisibility(View.GONE);
-
-
-        if (item.getState() == complete || item.getState() == waiteEvaluate)
-            order_complete_layout.setVisibility(View.VISIBLE);
-        else
-            order_complete_layout.setVisibility(View.GONE);
-        if (item.getInquisitionType() == 0) {
-            diagnoses_type_tv.setText("图文问诊");
-            Drawable leftimg = mContext.getResources().getDrawable(R.mipmap.photo_diagnoses_icon);
-            leftimg.setBounds(0, 0, leftimg.getMinimumWidth(), leftimg.getMinimumHeight());
-            diagnoses_type_tv.setCompoundDrawables(leftimg, null, null, null);
+            waitPeopleTv.setVisibility(View.VISIBLE);
+            waitPeopleTv.setText("前面待诊人数:" + item.getWaitInquiryCount());
         } else {
-            diagnoses_type_tv.setText("视频问诊");
-            Drawable leftimg = mContext.getResources().getDrawable(R.mipmap.video_diagnoses_icon);
-            leftimg.setBounds(0, 0, leftimg.getMinimumWidth(), leftimg.getMinimumHeight());
-            diagnoses_type_tv.setCompoundDrawables(leftimg, null, null, null);
+            waitPeopleTv.setVisibility(View.GONE);
+        }
+        if (item.getState() == unPay || item.getState() == waiteRecieve || item.getState() == changDoctor) {
+            backDiagnosesTv.setVisibility(View.VISIBLE);
+        } else {
+            backDiagnosesTv.setVisibility(View.GONE);
+        }
+        TextView payTv = helper.getView(R.id.pay_tv);
+        payTv.setOnClickListener(new View.OnClickListener() {
+            @SingleClick(1000)
+            @Override
+            public void onClick(View view) {
+                if (iOnItemBtnClickListener != null) {
+                    iOnItemBtnClickListener.onPayClick(item);
+                }
+            }
+        });
+        if (item.getState() == unPay) {
+            payTv.setVisibility(View.VISIBLE);
+        } else if (item.getState() != unPay && item.getIsSubOrderUnPay() == 1) {
+            payTv.setVisibility(View.VISIBLE);
+        } else {
+            payTv.setVisibility(View.GONE);
+        }
+
+        //        if(item.getState()==1)
+        //            wait_people_tv.setVisibility(View.VISIBLE);
+        //        else
+        //            wait_people_tv.setVisibility(View.GONE);
+        if (item.getState() == changDoctor) {
+            changeDiagnosesTv.setVisibility(View.VISIBLE);
+        } else {
+            changeDiagnosesTv.setVisibility(View.GONE);
+        }
+
+        if (item.getState() == waiteChangeDiagnose) {
+            diagnosesDoctorChangeTv.setVisibility(View.VISIBLE);
+        } else {
+            diagnosesDoctorChangeTv.setVisibility(View.GONE);
+        }
+        TextView commentTv = helper.getView(R.id.comment_tv);
+        commentTv.setOnClickListener(new View.OnClickListener() {
+            @SingleClick(1000)
+            @Override
+            public void onClick(View view) {
+                if (iOnItemBtnClickListener != null) {
+                    iOnItemBtnClickListener.onCommentClick(item);
+                }
+            }
+        });
+
+        if (item.getState() == waiteEvaluate) {
+            commentTv.setVisibility(View.VISIBLE);
+        } else {
+            commentTv.setVisibility(View.GONE);
+        }
+
+        LinearLayout stateLayout = helper.getView(R.id.state_layout);
+        if (item.getState() == cancel) {
+            stateLayout.setVisibility(View.VISIBLE);
+            TextView stateTv = helper.getView(R.id.state_tv);
+            if (item.getRefundState() == 0) {
+                stateTv.setText("已取消");
+            } else if (item.getRefundState() == 1) {
+                stateTv.setText("退款中");
+            } else {
+                stateTv.setText("已退款");
+            }
+        } else {
+            stateLayout.setVisibility(View.GONE);
+        }
+
+
+        if (item.getState() == complete || item.getState() == waiteEvaluate) {
+            orderCompleteLayout.setVisibility(View.VISIBLE);
+        } else {
+            orderCompleteLayout.setVisibility(View.GONE);
+        }
+        if (item.getInquisitionType() == 0) {
+            diagnosesTypeTv.setText("图文问诊");
+            Drawable leftImg = mContext.getResources().getDrawable(R.mipmap.photo_diagnoses_icon);
+            leftImg.setBounds(0, 0, leftImg.getMinimumWidth(), leftImg.getMinimumHeight());
+            diagnosesTypeTv.setCompoundDrawables(leftImg, null, null, null);
+        } else {
+            diagnosesTypeTv.setText("视频问诊");
+            Drawable leftImg = mContext.getResources().getDrawable(R.mipmap.video_diagnoses_icon);
+            leftImg.setBounds(0, 0, leftImg.getMinimumWidth(), leftImg.getMinimumHeight());
+            diagnosesTypeTv.setCompoundDrawables(leftImg, null, null, null);
         }
         //        if (TextUtils.isEmpty(item.getGroupTid())) {
         helper.setText(R.id.doctor_name_tv, item.getDoctorName());
