a1f3701b3c00b16e43c673a2d2c1fd220fcf75ad
dundun <371523912@qq.com>
2021-03-09 16:22:30 +0800

    feat[患者端]：1、开通健康管理服务首页完成

--- ih_patient/src/main/java/com/keydom/mianren/ih_patient/activity/health_manager/controller/HealthManagerController.java
+++ ih_patient/src/main/java/com/keydom/mianren/ih_patient/activity/health_manager/controller/HealthManagerController.java
@@ -3,8 +3,6 @@ package com.keydom.mianren.ih_patient.activity.health_manager.controller;
 import android.view.View;
 
 import com.keydom.ih_common.base.ControllerImpl;
-import com.keydom.mianren.ih_patient.R;
-import com.keydom.mianren.ih_patient.activity.diagnose_main.DiagnoseMainActivity;
 import com.keydom.mianren.ih_patient.activity.health_manager.view.HealthManagerView;
 
 /**
@@ -14,16 +12,6 @@ import com.keydom.mianren.ih_patient.activity.health_manager.view.HealthManagerV
 public class HealthManagerController extends ControllerImpl<HealthManagerView> implements View.OnClickListener {
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.iv_back:
-                getView().finishPage();
-                break;
-            case R.id.iv_online:
-                DiagnoseMainActivity.start(getContext());
-                break;
-            default:
-                break;
-        }
     }
 
     public void getHealthManagerData() {
--- ih_patient/src/main/java/com/keydom/mianren/ih_patient/activity/health_manager/view/HealthManagerView.java
+++ ih_patient/src/main/java/com/keydom/mianren/ih_patient/activity/health_manager/view/HealthManagerView.java
@@ -3,11 +3,11 @@ package com.keydom.mianren.ih_patient.activity.health_manager.view;
 import com.keydom.ih_common.base.BaseView;
 
 /**
+ * @author 顿顿
  * @date 20/3/4 10:55
  * @des 健康管理首页
  */
 public interface HealthManagerView extends BaseView {
     void requestSuccess();
 
-    void finishPage();
 }
